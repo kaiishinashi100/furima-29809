939a38a591fadaaaad5433f7758b3f9a18f56017
kaiishinashi100 <70202941+kaiishinashi100@users.noreply.github.com>
2020-09-16 12:43:27 +0900

    deviseによるユーザー管理機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,25 @@
 class ItemsController < ApplicationController
-
+  before_action :move_to_index, except: [:index]
   def index
- 
+      @user = User.all
   end
-  
+     
+
   def new
   end
 
+  def show
+  end
+
+  def edit
+  end
+
+  private
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
+
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 
-  root 'items#index'
-  get '/items', to: 'items#index'
+  root to: 'items#index'
+ 
+  
 
-  resources :imtes
+  resources :items do
+  end
+  
 
 end
