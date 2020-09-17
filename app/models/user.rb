a3463149a7f25378b01dbc32e3514f1fa9ae73cd
kaiishinashi100 <70202941+kaiishinashi100@users.noreply.github.com>
2020-09-17 11:33:14 +0900

    userモデル単体テスト実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,24 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true, length: { maximum: 10 }
+
+  validates :email, presence: true,format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, essage: "正しく入力してください"}
+
+  validates :password, presence: true,
+            format: { with: /\A[a-z0-9]+\z/i, message: "英数字を含めてください" }
+
+  validates :family_name_full_width,
+            :first_name_full_width, 
+            presence: true,
+            format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "全角文字を使用してください" }
+
+  validates :family_name_kana,
+            :first_name_kana,
+            presence: true,
+            format: { with: /\A([ァ-ン]|ー)+\z/, message: "カタカナで入力してください"}
+
+  validates :birth_day,
+            presence: true
 end
---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname                {Faker::Name.initials(number: 2)}
+    email                   {Faker::Internet.free_email}
+    password                {Faker::Internet.password(min_length: 6)}
+    password_confirmation   {password}
+    family_name_full_width  {"いい"}
+    first_name_full_width   {"うう"}
+    family_name_kana        {"イイ"}
+    first_name_kana         {"ウウ"}
+    birth_day              {Faker::Date.between(from: '2014-09-23', to: '2014-09-25')}
+  end
+end
